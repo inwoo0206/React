eed74889e7295310aac811915571f4c012f044df
권인우 <138741672+inwoo0206@users.noreply.github.com>
2024-01-26 16:55:47 +0900

    React Study

--- chaptor_8/src/Info.js
+++ chaptor_8/src/Info.js
@@ -3,77 +3,81 @@
 import { useState } from "react";
 
 const Info = () => {
-    const [name, setName] = useState('');
-    const [nickname, setNickname] = useState('');
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
 
-    const onChangeName = e => {
-        setName(e.target.value) 
-    }
-    const onChangeNickname = e => {
-        setNickname(e.target.value) 
-    }
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
 
-    return (
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
         <div>
-            <div>
-                <input value = {name} onChange={onChangeName} />
-                <input value = {nickname} onChange={onChangeNickname} />
-            </div>
-            <div>
-                <div>
-                    <b>이름:</b>{name}
-                </div>
-                <div>
-                    <b>닉네임:</b>{nickname}
-                </div>
-            </div>
+          <b>이름:</b>
+          {name}
         </div>
-    )
+        <div>
+          <b>닉네임:</b>
+          {nickname}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
 */
-/*
+
 // 8.2 useEffect 사용하기
 import { useState, useEffect } from "react";
 
 const Info = () => {
-    const [name, setName] = useState('');
-    const [nickname, setNickname] = useState('');
-    useEffect(() => {
-        console.log('effect');
-        return () => {
-            console.log('unmount');
-        };
-    },[]);
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+  useEffect(() => {
+    console.log("effect");
+    return () => {
+      console.log("unmount");
+    };
+  }, []);
 
-    const onChangeName = e => {
-        setName(e.target.value) 
-    }
-    const onChangeNickname = e => {
-        setNickname(e.target.value) 
-    }
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
 
-    return (
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
         <div>
-            <div>
-                <input value = {name} onChange={onChangeName} />
-                <input value = {nickname} onChange={onChangeNickname} />
-            </div>
-            <div>
-                <div>
-                    <b>이름:</b>{name}
-                </div>
-                <div>
-                    <b>닉네임:</b>{nickname}
-                </div>
-            </div>
+          <b>이름:</b>
+          {name}
         </div>
-    )
+        <div>
+          <b>닉네임:</b>
+          {nickname}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
-*/
+
 /*
 // 8.3.2 인풋 상태 관리하기
 import { useReducer } from "react";
@@ -115,6 +119,7 @@ const Info = () => {
 
 export default Info;
 */
+/*
 // 8.7 커스텀 Hooks 만들기 에서 만든 useInputs 사용해보기
 import useInputs from "./useInputs";
 
@@ -144,3 +149,4 @@ const Info = () => {
 };
 
 export default Info;
+*/
