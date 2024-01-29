4a9b94387f3a5fe76ae22ce3bc30be5d0f4c2dca
권인우 <138741672+inwoo0206@users.noreply.github.com>
2024-01-29 23:51:47 +0900

    react study

--- chaptor_8/src/Average.js
+++ chaptor_8/src/Average.js
@@ -43,48 +43,48 @@ const Average = () => {
 
 export default Average;
 */
-// 8.6 useRef 
+// 8.6 useRef
 import { useState, useMemo, useCallback, useRef } from "react";
 
-const getAverage = numbers => {
-    console.log('평균값 계산 중..');
-    if(numbers.length === 0) return 0;
-    const sum = numbers.reduce((a,b)=> a+ b);
-    return sum / numbers.length;
+const getAverage = (numbers) => {
+  console.log("평균값 계산 중..");
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((a, b) => a + b);
+  return sum / numbers.length;
 };
 
 const Average = () => {
-    const [list, setList] = useState([]);
-    const [number, setNumber] = useState('');
-    const inputEl = useRef(null);
+  const [list, setList] = useState([]);
+  const [number, setNumber] = useState("");
+  const inputEl = useRef(null);
 
-    const onChange = useCallback(e => {
-        setNumber(e.target.value);
-    },[]); // 컴포넌트가 처음 렌더링 될때만 함수 생성
+  const onChange = useCallback((e) => {
+    setNumber(e.target.value);
+  }, []); // 컴포넌트가 처음 렌더링 될때만 함수 생성
 
-    const onInsert = useCallback(() => {
-        const nextList = list.concat(parseInt(number));
-        setList(nextList);
-        setNumber('');
-        inputEl.current.focus();
-    }, [number,list]); // number 혹은 list 가 바뀌었을 때만 함수 생성
+  const onInsert = useCallback(() => {
+    const nextList = list.concat(parseInt(number));
+    setList(nextList);
+    setNumber("");
+    inputEl.current.focus();
+  }, [number, list]); // number 혹은 list 가 바뀌었을 때만 함수 생성
 
-    const avg = useMemo(()=> getAverage(list), [list]);
+  const avg = useMemo(() => getAverage(list), [list]);
 
-    return (
-        <div>
-            <input value = {number} onChange={onChange} ref = {inputEl} />
-            <button onClick={onInsert}>등록</button>
-            <ul>
-                {list.map((value,index) => (
-                    <li key={index}>{value}</li>
-                ))}
-            </ul>
-            <div>
-                <b>평균값:</b> {avg}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <input value={number} onChange={onChange} ref={inputEl} />
+      <button onClick={onInsert}>등록</button>
+      <ul>
+        {list.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+      <div>
+        <b>평균값:</b> {avg}
+      </div>
+    </div>
+  );
 };
 
 export default Average;
