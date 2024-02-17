5f296d681f8a9ed6aa525fd9a08577f1235c10f9
권인우 <138741672+inwoo0206@users.noreply.github.com>
2024-02-17 17:43:07 +0900

    react study

--- chaptor_17/src/App.js
+++ chaptor_17/src/App.js
@@ -1,3 +1,4 @@
+/*
 import Counter from './components/Counter';
 import Todos from './components/Todos';
 
@@ -5,6 +6,22 @@ const App = () => {
   return (
     <div>
       <Counter number={0} />
+      <hr />
+      <Todos />
+    </div>
+  );
+};
+
+export default App;
+*/
+// 17.5 컨테이너 컴포넌트 만들기
+import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
+
+const App = () => {
+  return (
+    <div>
+      <CounterContainer />
       <hr />
       <Todos />
     </div>
---
+++ chaptor_17/src/containers/CounterContainer.js
@@ -0,0 +1,47 @@
+/* 17.5
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+const mapStateToProps = (state) => ({
+  number: state.counter.number,
+});
+const mapDispatchToProps = (dispatch) => ({
+  // 임시함수
+  increase: () => {
+    console.log('increase');
+  },
+  decrease: () => {
+    console.log('decrease');
+  },
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+*/
+// 17.5 console.log 대신 액션 생성 함수 불러오가
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+const mapStateToProps = (state) => ({
+  number: state.counter.number,
+});
+const mapDispatchToProps = (dispatch) => ({
+  increase: () => {
+    dispatch(increase());
+  },
+  decrease: () => {
+    dispatch(decrease());
+  },
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
