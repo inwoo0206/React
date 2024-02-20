933684e0dfa9fce471ecc70f86f177e97b8d599d
권인우 <138741672+inwoo0206@users.noreply.github.com>
2024-02-20 19:32:11 +0900

    react study

--- chaptor_17/src/containers/CounterContainer.js
+++ chaptor_17/src/containers/CounterContainer.js
@@ -22,6 +22,7 @@ const mapDispatchToProps = (dispatch) => ({
 });
 export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
 */
+/*
 // 17.5 console.log 대신 액션 생성 함수 불러오가
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
@@ -45,3 +46,76 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+*/
+
+/*
+// 17.5 더 깔끔한 코드
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+export default connect(
+    state => ({
+        number: state.counter.number,
+    }),
+    dispatch => ({
+        increase: () => dispatch(increase()),
+        decrease: () => dispatch(decrease()),
+    })
+)(CounterContainer);
+*/
+
+/*
+// bindActionCreators 유틸 함수 사용하기
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+export default connect(
+    state => ({
+        number: state.counter.number,
+    }),
+    dispatch => 
+    bindActionCreators(
+        {
+        increase,
+        decrease,
+    },
+    dispatch,
+    ),
+)(CounterContainer);
+*/
+
+// mapDispatchToProps 에 해당 파라미터를 객체 형태로 넣어주기
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  {
+    increase,
+    decrease,
+  },
+)(CounterContainer);
