070673051faae3ed2e3aaa7c0c1c0244da38ded9
권인우 <138741672+inwoo0206@users.noreply.github.com>
2024-03-01 01:54:41 +0900

    react study

---
+++ blog-frontend/src/components/auth/AuthTemplate.js
@@ -0,0 +1,67 @@
+/*
+import styled from 'styled-components';
+
+/**
+ * 회원가입 / 로그인 페이지의 레이아웃을 담당하는 컴포넌트 이다.
+
+
+const AuthTemplateBlock = styled.div``;
+
+const AuthTemplate = ({children}) => {
+  return <AuthTemplateBlock>{children}</AuthTemplateBlock>;
+};
+
+export default AuthTemplate;
+*/
+
+// AuthTemplate 완성하기
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import { Link } from 'react-router-dom';
+
+/* 화면 전체를 채움 */
+const AuthTemplateBlock = styled.div`
+position: absolute;
+left: 0;
+top: 0;
+bottom: 0;
+right: 0;
+background: ${palette.gray[2]};
+/* flex 로 내부 내용 중앙 정렬 */
+display:flex;
+flex-direction:column;
+justify-content: center;
+align-items: center
+`;
+
+/* 흰색 박스 */
+const WhiteBox = styled.div`
+    .logo-area {
+        display: block;
+        padding-bottom: 2rem;
+        text-align: center;
+        font-weight: bold;
+        letter-spacing: 2px;
+    }
+    box-shadow: 0 0 8px rgba(0,0,0,0.025);
+    padding: 2rem;
+    width: 360px;
+    background: white;
+    border-radius: 2px;
+`;
+
+const AuthTemplate = ({children}) => {
+  return (
+    <AuthTemplateBlock>
+    <WhiteBox>
+    <div className='logo-area'>
+    <Link to='/'>REACTERS</Link>
+    </div>
+    {children}
+    </WhiteBox>
+    </AuthTemplateBlock>
+
+  )
+};
+
+export default AuthTemplate;
--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -9,4 +10,27 @@ root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
+);
+*/
+
+// 리덕스 적용하기
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from '../node_modules/redux-devtools-extension/index';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
